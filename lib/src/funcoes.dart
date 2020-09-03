53647d6bef9b84ebbc6c2ddbb56041d8ddc7a682
Alekissandro R. V. de Oliveira <alek.rossi@gmail.com>
2020-09-03 16:21:57 -0300

    On branch master  Changes to be committed: 	modified:   lib/src/funcoes.dart

--- lib/src/funcoes.dart
+++ lib/src/funcoes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:nibCuritiba/src/senibWidget.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'dart:async';
 
 launchURL_Facebook() async {
   const url = 'https://www.facebook.com/nibcuritiba/?ref=bookmarks';
@@ -30,7 +32,8 @@ launchURLInstagramNIB() async {
 }
 
 launchURLMaps() async {
-  const url = 'https://www.google.com.br/maps/place/Nova+Igreja+Batista+(NIB)/@-25.4966247,-49.3066172,17z/data=!3m1!4b1!4m5!3m4!1s0x94dce335f49cb917:0x1d54532da43316a3!8m2!3d-25.4966296!4d-49.3044285?fbclid=IwAR3MMPgHCmYPIhni0-70FLb1onTCfoqgCQWcHHLIgvhen_q77MoZWSGrnsI';
+  const url =
+      'https://www.google.com.br/maps/place/Nova+Igreja+Batista+(NIB)/@-25.4966247,-49.3066172,17z/data=!3m1!4b1!4m5!3m4!1s0x94dce335f49cb917:0x1d54532da43316a3!8m2!3d-25.4966296!4d-49.3044285?fbclid=IwAR3MMPgHCmYPIhni0-70FLb1onTCfoqgCQWcHHLIgvhen_q77MoZWSGrnsI';
   if (await canLaunch(url)) {
     await launch(url);
   } else {
@@ -39,16 +42,31 @@ launchURLMaps() async {
 }
 
 callMe() async {
-    // Android
-    const uri = 'tel:+554134026747';
-    if (await canLaunch(uri)) {
-      await launch(uri);
-    } else {
-        throw 'Could not launch $uri';
-      }
-    }
+  // Android
+  const uri = 'tel:+554134026747';
+  if (await canLaunch(uri)) {
+    await launch(uri);
+  } else {
+    throw 'Could not launch $uri';
+  }
+}
 
-   
-   
+send(mybody) async {
+  final Email email = Email(
+    body: mybody,
+    subject: 'Inscrição',
+    recipients: ['alek.rossi@gmail.com'],
+    isHTML: false,
+  );
 
-  
+  await FlutterEmailSender.send(email);
+}
+
+materialSenib() async {
+  const url = 'https://www.dropbox.com/home/SENIB';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch GoogleMaps';
+  }
+}
