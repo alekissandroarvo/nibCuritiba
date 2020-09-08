2a520e0379460a075f0f2b918d5a00a8125837b6
Alekissandro R. V. de Oliveira <alek.rossi@gmail.com>
2020-09-08 09:07:17 -0300

    On branch master  Changes to be committed: 	modified:   lib/src/nibMainWidget.dart

--- lib/src/nibMainWidget.dart
+++ lib/src/nibMainWidget.dart
@@ -53,159 +53,169 @@ class NibState extends State<NibStatefulWidget> {
       ),
 
       //Body
-      body: Column(
-        children: <Widget>[
-          //Row para ver endereço e link visitas
-          Row(
-            children: <Widget>[
-              Container(
-                child: Image.asset('imagens/predioNib.jpg'),
-                margin: const EdgeInsets.all(10.0),
-                width: 120.0,
-                height: 140.0,
-              ),
-              Expanded(
-                child: FlatButton.icon(
-                  //color: Colors.amber,
-                  onPressed: launchURLMaps,
-                  icon: Icon(Icons.navigation),
-                  label: Text("Visite"),
-                ),
-              ),
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("imagens/nib.jpeg"),
+            fit: BoxFit.cover,
+            colorFilter: new ColorFilter.mode(
+                Colors.black.withOpacity(0.2), BlendMode.dstATop),
           ),
+        ),
+        child: Column(
+          children: <Widget>[
+            //Row para ver endereço e link visitas
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Image.asset('imagens/predioNib.jpg'),
+                  margin: const EdgeInsets.all(10.0),
+                  width: 120.0,
+                  height: 140.0,
+                ),
+                Expanded(
+                  child: FlatButton.icon(
+                    //color: Colors.amber,
+                    onPressed: launchURLMaps,
+                    icon: Icon(Icons.navigation),
+                    label: Text("Visite"),
+                  ),
+                ),
+              ],
+            ),
 
-          //Row Endereço
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                    'R. Laudelino Ferreira Lopes-547- Bairro Novo-Curitiba-PR',
+            //Row Endereço
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                      'R. Laudelino Ferreira Lopes-547- Bairro Novo-Curitiba-PR',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 48.0,
+                  height: 40.0,
+                ),
+              ],
+            ),
+
+            //Row de versículo
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "E perseveravam no ensino dos apóstolos, e na comunhão, no partir do pão e nas orações. At 2.42",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        fontStyle: FontStyle.italic),
                     textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Container(
-                width: 48.0,
-                height: 40.0,
-              ),
-            ],
-          ),
-
-          //Row de versículo
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  "E perseveravam no ensino dos apóstolos, e na comunhão, no partir do pão e nas orações. At 2.42",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      fontStyle: FontStyle.italic),
-                  textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
 
-          Row(
-            children: <Widget>[
-              Container(
-                width: 48.0,
-                height: 80.0,
-              ),
-            ],
-          ),
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 48.0,
+                  height: 80.0,
+                ),
+              ],
+            ),
 
-          //Row para contactar
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Text('ENTRE EM CONTATO',
+            //Row para contactar
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text('ENTRE EM CONTATO',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton.icon(
+                    //color: Colors.amber,
+                    onPressed: callMe,
+                    icon: Icon(Icons.call),
+                    label: Text("Ligue"),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Container(
+                      child: Image.asset('imagens/whatsup.jpg'),
+                      margin: const EdgeInsets.all(10.0),
+                      width: 50.0,
+                      height: 50.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            //Midias Sociais
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    "MÍDIAS SOCIAIS",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                     textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: FlatButton.icon(
-                  //color: Colors.amber,
-                  onPressed: callMe,
-                  icon: Icon(Icons.call),
-                  label: Text("Ligue"),
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Container(
-                    child: Image.asset('imagens/whatsup.jpg'),
-                    margin: const EdgeInsets.all(10.0),
-                    width: 50.0,
-                    height: 50.0,
                   ),
                 ),
-              ),
-            ],
-          ),
-
-          //Midias Sociais
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  "MÍDIAS SOCIAIS",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  onPressed: launchURL_Facebook,
-                  child: Container(
-                    child: Image.asset('imagens/facebook.png'),
-                    margin: const EdgeInsets.all(10.0),
-                    width: 40.0,
-                    height: 40.0,
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: launchURL_Facebook,
+                    child: Container(
+                      child: Image.asset('imagens/facebook.png'),
+                      margin: const EdgeInsets.all(10.0),
+                      width: 40.0,
+                      height: 40.0,
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  onPressed: () {},
-                  child: Container(
-                    child: Image.asset('imagens/twitter.jpg'),
-                    margin: const EdgeInsets.all(10.0),
-                    width: 40.0,
-                    height: 40.0,
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Container(
+                      child: Image.asset('imagens/twitter.jpg'),
+                      margin: const EdgeInsets.all(10.0),
+                      width: 40.0,
+                      height: 40.0,
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  onPressed: launchURLInstagramNIB,
-                  child: Container(
-                    child: Image.asset('imagens/instagram.jpg'),
-                    margin: const EdgeInsets.all(10.0),
-                    width: 40.0,
-                    height: 40.0,
+                Expanded(
+                  child: FlatButton(
+                    onPressed: launchURLInstagramNIB,
+                    child: Container(
+                      child: Image.asset('imagens/instagram.jpg'),
+                      margin: const EdgeInsets.all(10.0),
+                      width: 40.0,
+                      height: 40.0,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
 
       drawer: Drawer(
@@ -294,10 +304,7 @@ class NibState extends State<NibStatefulWidget> {
                 style: TextStyle(height: 5, fontSize: 14),
               ),
               onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
+                eventosNib();
               },
             ),
           ],
